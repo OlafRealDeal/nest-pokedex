9f2b09cfc58a6509eb4d0fa12b2324f28d4a2102
Alvaro Flores <almaflomon@gmail.com>
2022-09-16 16:02:52 -0400

    Deploy intento 6

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v2')
 
 
-  await app.listen( process.env.PORT ) || 3000, 
+  app.listen( process.env.PORT ) || 3000, 
     
   console.log( `App running on port ${ process.env.PORT}` );
   
