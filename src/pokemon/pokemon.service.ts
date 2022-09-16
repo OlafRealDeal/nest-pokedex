7e901a493ea86e80f8aa9f4a49c914f6c354d231
Alvaro Flores <almaflomon@gmail.com>
2022-09-16 12:25:16 -0400

    Primer Deploy

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,15 +5,25 @@ import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
 
-  ) {}
+    private readonly configService: ConfigService,
+    
+
+  ) {
+    
+    this.defaultLimit =  configService.get<number>('defaultLimit');
+    
+  }
 
 
 
@@ -27,17 +37,16 @@ export class PokemonService {
       
     } catch (error) {
       this.handleExceptions( error );
-      
-      
-      
     }
-    
     
   }
   
   findAll( paginationDto: PaginationDto) {
 
-    const { limit = 10, offset= 0 } = paginationDto;
+    
+    
+
+    const { limit = this.defaultLimit , offset= 0 } = paginationDto;
 
     return this.pokemonModel.find()
     .limit( limit ) 
