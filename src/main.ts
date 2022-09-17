0da6800a2b65c799b3052308388f2b6d47632edf
Alvaro Flores <almaflomon@gmail.com>
2022-09-16 23:14:07 -0400

    Segundo intento 4

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v2')
   
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,7 +17,6 @@ async function bootstrap() {
       }),
   )
 
-  app.setGlobalPrefix('api/v2')
 
 
   await app.listen( process.env.PORT ), 
